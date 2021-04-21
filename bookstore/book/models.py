2766ef130c775e81664d7a711ce643e87bd827fb
abdelrahmany0 <abdelrhmanatta01@gmail.com>
2021-04-21 16:25:24 +0200

    adding admin dashboard & control

--- bookstore/book/models.py
+++ bookstore/book/models.py
@@ -4,3 +4,6 @@ from django.db import models
 class Book(models.Model):
     title = models.CharField(max_length=40)
     content = models.TextField(max_length=2048)
+
+    def __str__(self):
+        return self.title
